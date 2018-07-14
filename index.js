8bfd93e75d4a6233b501514b7a3d2bba25b76652
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-14 15:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function multiply(a, b) { return (a * b) }
 function divide(a, b) { return (a/b) }
 function inc(n) { return (n + 1) }
 function dec(n) { return (n - 1) }
+var n = '0'
 function makeInt(n) { parseInt(n, 10) }
 function preserveDecimal(n) { parseFloat(n) }
